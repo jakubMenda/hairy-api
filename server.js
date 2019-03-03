9285cd9e2cc9301f54f32e8efbc39f4585112539
Jakub Menda <jakubmenda@gmail.com>
2019-03-03 16:13:26 +0100

    deployment fixes

--- server.js
+++ server.js
@@ -8,22 +8,22 @@ app.use(bodyParser.json());
 
 let db;
 
-mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", function (err, client) {
-  if (err) {
-    console.log(err);
-    process.exit(1);
-  }
-
-  // Save database object from the callback for reuse.
-  db = client.db();
-  console.log("Database connection ready");
-
-  // Initialize the app.
-  var server = app.listen(process.env.PORT || 8080, function () {
-    var port = server.address().port;
-    console.log("App now running on port", port);
-  });
-});
+// mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", function (err, client) {
+//   if (err) {
+//     console.log(err);
+//     process.exit(1);
+//   }
+//
+//   // Save database object from the callback for reuse.
+//   db = client.db();
+//   console.log("Database connection ready");
+//
+//   // Initialize the app.
+//   var server = app.listen(process.env.PORT || 8080, function () {
+//     var port = server.address().port;
+//     console.log("App now running on port", port);
+//   });
+// });
 
 function handleError(res, reason, message, code) {
   console.log("ERROR: " + reason);
