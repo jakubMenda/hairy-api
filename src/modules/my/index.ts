8090749ef869953a726d8cd387523f719d1bdce9
Jakub Menda <jakubmenda@gmail.com>
2019-03-11 10:03:48 +0100

    PUT /my/user

--- src/modules/my/index.ts
+++ src/modules/my/index.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response, Router } from 'express';
 import { getRequestingUser } from '../../utils/authentication';
-import { OK } from 'http-codes';
+import { BAD_REQUEST, NOT_FOUND, OK } from 'http-codes';
+import { HttpError } from '../../utils/errorHandling/errors';
 
 const myController = Router();
 
@@ -9,9 +10,48 @@ myController.get('/user', async (req: Request, res: Response, next: NextFunction
     const token = req.header('Authorization');
     const user = await getRequestingUser(token);
 
+    if (!user) {
+      throw new HttpError({
+        statusCode: NOT_FOUND,
+        message: 'User not found',
+      });
+    }
     res.status(OK).json(await user.getPublicProfile());
     next();
   } catch (e) {
+    return next(e);
+  }
+});
+
+myController.put('/user', async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['firstName', 'lastName', 'password'];
+    const isValidOperation = updates.every((update: string) => allowedUpdates.includes(update));
+
+    if (!isValidOperation) {
+      throw new HttpError({
+        statusCode: BAD_REQUEST,
+        message: 'Bad request',
+      });
+    }
+
+    const token = req.header('Authorization');
+    const user: any = await getRequestingUser(token);
+
+    if (!user) {
+      throw new HttpError({
+        statusCode: NOT_FOUND,
+        message: 'User not found',
+      });
+    }
+
+    updates.forEach((update: string) => user[update] = req.body[update]);
+    const updatedUser = await user.save();
+
+    res.status(OK).json(await updatedUser.getPublicProfile());
+    next();
+  } catch(e) {
     return next(e);
   }
 });
